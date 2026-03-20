e0491b38a39570884eb5dc6d2598041aca231661
subs67023-stack <subs67023@gmail.com>
2026-03-20 12:49:56 +0530

    solution from 11 to 15

---
+++ checkVotingEligibility.js
@@ -0,0 +1,15 @@
+// Write a function to check if the user is eligible to vote.
+// Instructions:
+// You can vote if you are 18 or above.
+// Return "Eligible" if you can vote else return "Not Eligible".
+// Example:
+// For this input age = 21, the result should be: "Eligible"
+// Reason: 21 is greater than 18.
+
+function checkVotingEligibility(age) {
+  if (age >= 18) {
+    return "Eligible";
+  } else {
+    return "Not Eligible";
+  }
+}
---
+++ minutesToSeconds.js
@@ -0,0 +1,11 @@
+// Write a function to convert minutes into seconds.
+// Instructions:
+// Return the total number of seconds for the given number of minutes.
+// Hint: 1 minute = 60 seconds
+// Example:
+// For this input minutes = 5, the result should be: 300
+// Reason: 5 multiplied by 60 is 300.
+
+function minutesToSeconds(minutes) {
+  return minutes * 60;
+}
