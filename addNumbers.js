110feacb721b99ba31eaaf5e89da7bf5a53b0d7e
subs67023-stack <subs67023@gmail.com>
2026-03-20 12:24:56 +0530

    Add addNumbers solution with full question text

---
+++ addNumbers.js
@@ -0,0 +1,10 @@
+// Write a function to add two numbers.
+// Instructions:
+// Return the sum of num1 and num2.
+// Example:
+// For this input num1 = 4, num2 = 5, the result should be: 9
+// Reason: Sum of 4 and 5 is 9.
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
